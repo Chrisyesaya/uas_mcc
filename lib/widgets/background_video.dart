8f70ae2d325ad90e767b4ec8fe8ac9f5d58fac4d
Chrisyesaya <chrisyesaya90@gmail.com>
2025-12-10 21:28:33 +0700

    video

--- lib/widgets/background_video.dart
+++ lib/widgets/background_video.dart
@@ -3,7 +3,13 @@ import 'package:video_player/video_player.dart';
 
 class BackgroundVideo extends StatefulWidget {
   final String assetPath;
-  const BackgroundVideo({super.key, required this.assetPath});
+  final double darkOpacity;
+
+  const BackgroundVideo({
+    super.key,
+    required this.assetPath,
+    this.darkOpacity = 0.4,
+  });
 
   @override
   State<BackgroundVideo> createState() => _BackgroundVideoState();
@@ -33,14 +39,24 @@ class _BackgroundVideoState extends State<BackgroundVideo> {
   @override
   Widget build(BuildContext context) {
     if (!_controller.value.isInitialized) return const SizedBox.shrink();
+
     return SizedBox.expand(
-      child: FittedBox(
-        fit: BoxFit.cover,
-        child: SizedBox(
-          width: _controller.value.size.width,
-          height: _controller.value.size.height,
-          child: VideoPlayer(_controller),
-        ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          FittedBox(
+            fit: BoxFit.cover,
+            child: SizedBox(
+              width: _controller.value.size.width,
+              height: _controller.value.size.height,
+              child: VideoPlayer(_controller),
+            ),
+          ),
+
+          Container(
+            color: Colors.black.withOpacity(widget.darkOpacity),
+          ),
+        ],
       ),
     );
   }
